a621f7e9a49be99bee06304733d2a8670ed6c16f
Jarrod O'Callaghan <12422677+JarrodOCallaghan@users.noreply.github.com>
2023-12-08 14:09:05 +1100

    D3p2 WIP

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -1,8 +1,11 @@
 use std::fs::read_to_string;
 mod p1;
+mod p2;
+
 fn main() {
     let data = read_data("data.txt");
     p1::solve(&data);
+    // p2::solve(&data);
 }
 
 fn read_data(filename: &str) -> Vec<Vec<char>> {
--- d03/src/p2.rs
+++ d03/src/p2.rs
@@ -4,11 +4,16 @@ struct Coordinate {
     col: i32,
 }
 
+use std::collections::HashMap;
+
 pub fn solve(data: &Vec<Vec<char>>) {
     let mut total: i32 = 0;
+    let mut gear_ratios: HashMap<Coordinate, Vec<i32>> = HashMap::new();
+
     for (i, row) in data.iter().enumerate() {
         let mut is_valid: bool = false;
         let mut number: i32 = 0;
+        let mut saved_coord: Coordinate;
         for (j, c) in row.iter().enumerate() {
             if c.is_digit(10) {
                 let n = c.to_digit(10).unwrap_or(0) as i32;
@@ -22,45 +27,49 @@ pub fn solve(data: &Vec<Vec<char>>) {
                     row: j as i32,
                 };
                 // print!("GOT HERE");
-                if check_perimeter_for_special_char(data, current) {
+                let (validity, coord) = check_perimeter_for_gear(data, current);
+                saved_coord = coord;
+
+                if validity {
                     is_valid = true;
-                    // println!("True");
+                    // gear_ratios.insert(coord, Vec::new());
                 }
-                // println!("{}", c);
+
             } else {
-                if number > 0 && is_valid {
-                    // println!("\nValid num: {number}");
-                }
                 if is_valid {
-                    total += number;
+                    // let mut key_vector: Vec<i32> = gear_ratios.get(&saved_coord);
+                    // key_vector.push(number);
+                    // gear_ratios.insert(saved_coord, key_vector);
                     is_valid = false;
                 }
                 number = 0
             }
         }
     }
-    println!("P1 Total: {total}");
+    println!("P2 total: {total}");
 }
 
-fn check_perimeter_for_special_char(data: &Vec<Vec<char>>, current: Coordinate) -> bool {
-    // println!("Char ?: {}", data[current.col as usize][current.row as usize]);
+
+fn check_perimeter_for_gear(data: &Vec<Vec<char>>, current: Coordinate) -> (bool, Coordinate) {
+    let mut validity: bool;
+
     for i in -1..2 {
         for j in -1..2 {
             let next_coord = Coordinate {
-                row: current.row + i,
-                col: current.col + j,
+                col: current.col + i,
+                row: current.row + j,
             };
+
             if is_coord_in_range(&next_coord, data.len(), data[0].len()) {
                 let c = data[next_coord.col as usize][next_coord.row as usize];
-                if !(c.is_digit(10)) && !(c == '.') && !(c == '\n') {
-                    // println!("CHAR: {c}: ");
-                    // println!("{} x {}", next_coord.col, next_coord.row);
-                    return true;
+                if c == '*'{
+                    return (true, next_coord);
                 }
             }
+
         }
     }
-    return false;
+    return (false, current);
 }
 
 fn is_coord_in_range(next_coord: &Coordinate, max_col: usize, max_row: usize) -> bool {
